742772b42cf33d177df3f619e8a601937457f9b9
geeta gohil <gitgohil@yahoo.co.in>
2023-02-25 14:19:53 +0000

    Architect1

---
+++ java/Variable1/Constant.java
@@ -0,0 +1,13 @@
+package Variable1;
+
+public class Constant {
+    static final String BANK_NAME = "Lloyds Bank";
+    static final int BANK_ACCOUNT_NUMBER = 20000000;
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
